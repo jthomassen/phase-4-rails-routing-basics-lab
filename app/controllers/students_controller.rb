98032cb42ba11a6e24aad29bc1c2afbdbb6c6d0d
Jeff Thomassen <jeff.thomassen@ucdenver.edu>
2021-09-20 11:11:59 -0600

    commit tree c6883bbaa8ea98b5df695423e0e94336a8f77092

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,16 @@
+class StudentsController < ApplicationController
+    def index
+        students = Student.all 
+        render json: students
+    end
+
+    def grades
+        grades = Student.order(:grade)
+        render json: grades
+    end
+
+    def highest_grade
+        highest_grade = Student.all.order(:grade).last
+        render json: highest_grade
+    end
+end
